4676c7f885c3f4d09e7f5384263a8f0d9217d5d4
tboy <tosinmoses384@gmail.com>
2025-01-02 07:07:08 +0100

    feat(authOnboardLand:complete app onboarding, landing page and auth pages

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,45 +1,171 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
+import { View, Text, StyleSheet } from "react-native";
+import React from "react";
+import { useTheme } from "@/context/theme.context";
+import useUser from "@/hooks/fetch/useUser";
+import { Tabs } from "expo-router";
+import { Feather, Ionicons, Octicons } from "@expo/vector-icons";
+import { moderateScale, scale, verticalScale } from "react-native-size-matters";
+import { fontSizes, IsAndroid, IsIOS, IsIPAD } from "@/themes/app.constant";
+import { BlurView } from "expo-blur";
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
-import { Colors } from '@/constants/Colors';
-import { useColorScheme } from '@/hooks/useColorScheme';
-
-export default function TabLayout() {
-  const colorScheme = useColorScheme();
+export default function _layout() {
+  const { theme } = useTheme();
+  const { loader } = useUser();
 
   return (
     <Tabs
-      screenOptions={{
-        tabBarActiveTintColor: Colors[colorScheme ?? 'light'].tint,
-        headerShown: false,
-        tabBarButton: HapticTab,
-        tabBarBackground: TabBarBackground,
-        tabBarStyle: Platform.select({
-          ios: {
-            // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+      screenOptions={({ route }) => {
+        return {
+          tabBarIcon: ({ color }) => {
+            let iconName;
+            if (route.name === "index") {
+              iconName = (
+                <Feather
+                  name="home"
+                  size={moderateScale(24)}
+                  style={{ width: IsIPAD ? scale(20) : "auto" }}
+                  color={color}
+                />
+              );
+            } else if (route.name === "courses/index") {
+              iconName = (
+                <Feather
+                  name="book-open"
+                  size={moderateScale(24)}
+                  style={{ width: IsIPAD ? scale(20) : "auto" }}
+                  color={color}
+                />
+              );
+            } else if (route.name === "resources/index") {
+              iconName = (
+                <Ionicons
+                  name="document-text-outline"
+                  size={moderateScale(24)}
+                  style={{ width: IsIPAD ? scale(20) : "auto" }}
+                  color={color}
+                />
+              );
+            } else if (route.name === "profile/index") {
+              iconName = (
+                <Octicons
+                  name="person"
+                  size={moderateScale(26)}
+                  style={{ width: IsIPAD ? scale(20) : "auto" }}
+                  color={color}
+                />
+              );
+            }
+            return iconName;
           },
-          default: {},
-        }),
-      }}>
-      <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="house.fill" color={color} />,
-        }}
-      />
-      <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
-        }}
-      />
+          tabBarActiveTintColor: theme.dark ? "#19C964" : "#4A90E2",
+          tabBarInactiveTintColor: theme.dark ? "#fff" : "#8e8e93",
+          headerShown:
+            route.name === "courses/index" || route.name === "resources/index"
+              ? true
+              : false,
+          headerTitle:
+            route.name === "courses/index"
+              ? "Courses"
+              : route.name === "resources/index"
+              ? "Video Lessons"
+              : "",
+          headerTitleStyle: {
+            color: theme.dark ? "#fff" : "#000",
+            textAlign: "center",
+            width: scale(320),
+            fontSize: fontSizes.FONT22,
+            fontFamily: "Poppins_400Regular",
+          },
+          headerBackgroundContainerStyle: {
+            backgroundColor: theme.dark ? "#131313" : "#fff",
+            shadowColor: theme.dark ? "#fff" : "#000",
+            shadowOpacity: theme.dark ? 0.1 : 0.1,
+            shadowOffset: { width: 0, height: 1 },
+            shadowRadius: 1,
+            elevation: 1,
+          },
+          headerBackground: () => (
+            <BlurView
+              intensity={theme.dark ? 70 : 80}
+              style={{
+                borderTopLeftRadius: scale(20),
+                borderTopRightRadius: scale(20),
+                overflow: "hidden",
+                backgroundColor: "transparent",
+              }}
+            />
+          ),
+          tabBarShowLabel: false,
+          tabBarStyle: {
+            position: IsIOS ? (theme.dark ? "absolute" : "static") : "absolute",
+            borderTopLeftRadius: IsAndroid ? 0 : IsIPAD ? scale(20) : scale(35),
+            borderTopRightRadius: IsAndroid
+              ? 0
+              : IsIPAD
+              ? scale(20)
+              : scale(35),
+            borderTopWidth: 0,
+            height: verticalScale(55),
+            opacity: loader ? 0 : 1,
+            transition: "opacity 0.3s ease-in-out",
+          },
+          tabBarBackground: () => {
+            return (
+              <>
+                {IsIOS && !theme.dark ? (
+                  <View
+                    style={{
+                      ...StyleSheet.absoluteFillObject,
+                      backgroundColor: "#fff",
+                      borderTopLeftRadius: IsAndroid
+                        ? 0
+                        : IsIPAD
+                        ? scale(25)
+                        : scale(35),
+                      borderTopRightRadius: IsAndroid
+                        ? 0
+                        : IsIPAD
+                        ? scale(25)
+                        : scale(35),
+                      overflow: "hidden",
+                    }}
+                  />
+                ) : (
+                  <BlurView
+                    intensity={theme.dark ? (IsAndroid ? 10 : 60) : 100}
+                    style={{
+                      ...StyleSheet.absoluteFillObject,
+                      borderTopLeftRadius: IsAndroid
+                        ? 0
+                        : IsIPAD
+                        ? scale(25)
+                        : scale(35),
+                      borderTopRightRadius: IsAndroid
+                        ? 0
+                        : IsIPAD
+                        ? scale(25)
+                        : scale(35),
+                      overflow: "hidden",
+                      backgroundColor: IsAndroid
+                        ? theme.dark
+                          ? "#131313"
+                          : "#fff"
+                        : theme.dark
+                        ? "transparent"
+                        : "#fff",
+                    }}
+                  />
+                )}
+              </>
+            );
+          },
+        };
+      }}
+    >
+      <Tabs.Screen name="index" />
+      <Tabs.Screen name="courses/index" />
+      <Tabs.Screen name="resources/index" />
+      <Tabs.Screen name="profile/index" />
     </Tabs>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,39 +1,35 @@
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { Stack } from 'expo-router';
-import * as SplashScreen from 'expo-splash-screen';
-import { StatusBar } from 'expo-status-bar';
-import { useEffect } from 'react';
-import 'react-native-reanimated';
-
-import { useColorScheme } from '@/hooks/useColorScheme';
+import React from "react";
+import { SplashScreen, Stack } from "expo-router";
+import { ThemeProvider, useTheme } from "@/context/theme.context";
+import {
+  Poppins_600SemiBold,
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_700Bold,
+  Poppins_500Medium,
+  useFonts,
+} from "@expo-google-fonts/poppins";
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
-SplashScreen.preventAutoHideAsync();
+// SplashScreen.preventAutoHideAsync();
 
-export default function RootLayout() {
-  const colorScheme = useColorScheme();
+export default function _layout() {
   const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
+    SpaceMono: require("../assets/fonts/SpaceMono-Regular.ttf"),
+    Poppins_600SemiBold,
+    Poppins_300Light,
+    Poppins_700Bold,
+    Poppins_400Regular,
+    Poppins_500Medium,
   });
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
-
-  if (!loaded) {
-    return null;
-  }
-
   return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="+not-found" />
+    <ThemeProvider>
+      <Stack screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="index" />
+        <Stack.Screen name="(routes)/onboarding/index" />
+        <Stack.Screen name="(routes)/notification/index" />
       </Stack>
-      <StatusBar style="auto" />
     </ThemeProvider>
   );
 }
